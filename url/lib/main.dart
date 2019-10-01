9021d201b6165f0ac17ed4b275bf2d2b2eee3c26
Surya Dev Singh <46717624+suryadevsingh@users.noreply.github.com>
2019-10-01 12:47:57 +0530

    first commit

---
+++ url/lib/main.dart
@@ -0,0 +1,35 @@
+ import 'package:flutter/material.dart';
+ import 'package:url_launcher/url_launcher.dart';
+
+ void main() {
+   runApp(MyApp());
+ }
+ class MyApp extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       
+       debugShowCheckedModeBanner: false,
+       home: DemoPage(),
+     );
+   }
+ }
+ class DemoPage extends StatelessWidget {
+   launchURL() {
+     launch('https://flutter.dev');
+   }
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       appBar: AppBar(
+         title: Text("flutter dev"),
+       ),
+       body: Center(
+         child: RaisedButton(
+           onPressed: launchURL,
+           child: Text('Show Flutter homepage'),
+         ),
+       ),
+     );
+   }
+ }
